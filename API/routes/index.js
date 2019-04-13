928e6e1403c3f04b8c1f505981904b9227fd50f8
allebd <delebella@yahoo.com>
2019-04-13 08:32:33 +0100

    feature(signin API):signin API endpoint with test [Finishes #165328363]

--- API/routes/index.js
+++ API/routes/index.js
@@ -5,6 +5,8 @@ const API_VERSION = '/api/v1';
 const route = (app) => {
   app.post(`${API_VERSION}/auth/signup`, userController.signup);
   app.all(`${API_VERSION}/auth/signup`, userController.invalidUserRequest);
+  app.post(`${API_VERSION}/auth/signin`, userController.signin);
+  app.all(`${API_VERSION}/auth/signin`, userController.invalidUserRequest);
 };
 
 export default route;
