58c481a46d267d0ebb2376c63ae6adeaed705b6b
allebd <delebella@yahoo.com>
2019-04-18 05:48:24 +0100

    chore(gh-pages): modify UI on gh-pages [Finishes #165437370]

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,90 +1,102 @@
-const adminlogin = document.getElementById("adminlogin");
-const positiveDelete = document.getElementById("positiveDelete");
-const negativeDelete = document.getElementById("negativeDelete");
-const positiveActivate = document.getElementById("positiveActivate");
-const negativeActivate = document.getElementById("negativeActivate");
-const positiveDeactivate = document.getElementById("positiveDeactivate");
-const negativeDeactivate = document.getElementById("negativeDeactivate");
+/* eslint-disable no-undef */
+/* global document */
+const adminlogin = document.getElementById('adminlogin');
+const positiveDelete = document.getElementById('positiveDelete');
+const negativeDelete = document.getElementById('negativeDelete');
+const positiveActivate = document.getElementById('positiveActivate');
+const negativeActivate = document.getElementById('negativeActivate');
+const positiveDeactivate = document.getElementById('positiveDeactivate');
+const negativeDeactivate = document.getElementById('negativeDeactivate');
+const alertMessage = document.querySelector('.alert');
 
-if(adminlogin){
-    adminlogin.addEventListener("click", (event) => {
-        event.preventDefault();
-        const adminrole = document.getElementById("adminrole").value;
-        if(adminrole === "staff"){
-            window.location.href = "staffdashboard.html";
-        }else{
-            window.location.href = "admindashboard.html";
-        }
-    });
-}
+if (adminlogin) {
+  adminlogin.addEventListener('click', (event) => {
+    event.preventDefault();
+    const adminrole = document.getElementById('adminrole').value;
+    const adminemail = document.getElementById('adminemail').value;
+    const adminpass = document.getElementById('adminpass').value;
 
-for(let i = 1; i < 6; i++){
-    let activateAccount = document.getElementById("activateAccount"+i);
-    let deactivateAccount = document.getElementById("deactivateAccount"+i);
-    let deleteAccount = document.getElementById("deleteAccount"+i);
-    if(activateAccount){
-        activateAccount.addEventListener("click", () =>{
-            document.getElementById('activate-account').style.display='block';
-        });
+    if (adminemail.trim('') !== '' && adminpass.trim('') !== '') {
+      if (adminrole === 'staff') {
+        window.location.href = 'staffdashboard.html';
+      } else {
+        window.location.href = 'admindashboard.html';
+      }
+    } else {
+      alertMessage.innerHTML = "<div class='alert-message alert-error'><p> Incorrect email or password, kindly try again.</a></p</div>";
+      setTimeout(() => { alertMessage.innerHTML = ''; }, 5000);
     }
-
-    if(deactivateAccount){
-        deactivateAccount.addEventListener("click", () => {
-            document.getElementById('deactivate-account').style.display='block';
-        });
-    }
-
-    if(deleteAccount){
-        deleteAccount.addEventListener("click", () => {
-            document.getElementById('delete-account').style.display='block';
-        });
-    }
+  });
 }
 
-if(positiveDelete){
-    positiveDelete.addEventListener("click", () => {
-        document.getElementById('activate-account').style.display='none';
-        document.getElementById('deactivate-account').style.display='none';
-        document.getElementById('delete-account').style.display='none';
+// eslint-disable-next-line no-plusplus
+for (let i = 1; i < 6; i++) {
+  const activateAccount = document.getElementById(`activateAccount${i}`);
+  const deactivateAccount = document.getElementById(`deactivateAccount${i}`);
+  const deleteAccount = document.getElementById(`deleteAccount${i}`);
+  if (activateAccount) {
+    activateAccount.addEventListener('click', () => {
+      document.getElementById('activate-account').style.display = 'block';
     });
+  }
+
+  if (deactivateAccount) {
+    deactivateAccount.addEventListener('click', () => {
+      document.getElementById('deactivate-account').style.display = 'block';
+    });
+  }
+
+  if (deleteAccount) {
+    deleteAccount.addEventListener('click', () => {
+      document.getElementById('delete-account').style.display = 'block';
+    });
+  }
 }
 
-if(negativeDelete){
-    negativeDelete.addEventListener("click", () => {
-        document.getElementById('activate-account').style.display='none';
-        document.getElementById('deactivate-account').style.display='none';
-        document.getElementById('delete-account').style.display='none';
-    });
+if (positiveDelete) {
+  positiveDelete.addEventListener('click', () => {
+    document.getElementById('activate-account').style.display = 'none';
+    document.getElementById('deactivate-account').style.display = 'none';
+    document.getElementById('delete-account').style.display = 'none';
+  });
 }
 
-if(positiveActivate){
-    positiveActivate.addEventListener("click", () => {
-        document.getElementById('activate-account').style.display='none';
-        document.getElementById('deactivate-account').style.display='none';
-        document.getElementById('delete-account').style.display='none';
-    });
+if (negativeDelete) {
+  negativeDelete.addEventListener('click', () => {
+    document.getElementById('activate-account').style.display = 'none';
+    document.getElementById('deactivate-account').style.display = 'none';
+    document.getElementById('delete-account').style.display = 'none';
+  });
 }
 
-if(negativeActivate){
-    negativeActivate.addEventListener("click", () => {
-        document.getElementById('activate-account').style.display='none';
-        document.getElementById('deactivate-account').style.display='none';
-        document.getElementById('delete-account').style.display='none';
-    });
+if (positiveActivate) {
+  positiveActivate.addEventListener('click', () => {
+    document.getElementById('activate-account').style.display = 'none';
+    document.getElementById('deactivate-account').style.display = 'none';
+    document.getElementById('delete-account').style.display = 'none';
+  });
 }
 
-if(positiveDeactivate){
-    positiveDeactivate.addEventListener("click", () => {
-        document.getElementById('activate-account').style.display='none';
-        document.getElementById('deactivate-account').style.display='none';
-        document.getElementById('delete-account').style.display='none';
-    });
+if (negativeActivate) {
+  negativeActivate.addEventListener('click', () => {
+    document.getElementById('activate-account').style.display = 'none';
+    document.getElementById('deactivate-account').style.display = 'none';
+    document.getElementById('delete-account').style.display = 'none';
+  });
 }
 
-if(negativeDeactivate){
-    negativeDeactivate.addEventListener("click", () => {
-        document.getElementById('activate-account').style.display='none';
-        document.getElementById('deactivate-account').style.display='none';
-        document.getElementById('delete-account').style.display='none';
-    });
+if (positiveDeactivate) {
+  positiveDeactivate.addEventListener('click', () => {
+    document.getElementById('activate-account').style.display = 'none';
+    document.getElementById('deactivate-account').style.display = 'none';
+    document.getElementById('delete-account').style.display = 'none';
+  });
+}
+
+if (negativeDeactivate) {
+  negativeDeactivate.addEventListener('click', () => {
+    document.getElementById('activate-account').style.display = 'none';
+    document.getElementById('deactivate-account').style.display = 'none';
+    document.getElementById('delete-account').style.display = 'none';
+  });
 }
