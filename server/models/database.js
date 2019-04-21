b685bcc6fd9a5ea63ec65c6726d3328cfc082c26
allebd <delebella@yahoo.com>
2019-04-21 04:02:06 +0100

    chore(database): setup database installed pg package setup database config create required table structure added default admin [Finishes #165444066]

---
+++ server/models/database.js
@@ -0,0 +1,20 @@
+/* eslint-disable prefer-destructuring */
+import Debug from 'debug';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+let DATABASE_URL;
+const debug = Debug('http');
+dotenv.config();
+
+if (process.env.NODE_ENV === 'TEST') {
+  DATABASE_URL = process.env.DATABASE_URL_TEST;
+} else {
+  DATABASE_URL = process.env.DATABASE_URL;
+}
+
+const pool = new Pool({ connectionString: DATABASE_URL });
+
+pool.on('connect', () => { debug('connected to the database'); });
+
+export default pool;
