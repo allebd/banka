dcab1683635267ba9b023c9e478e4e49be569251
allebd <delebella@yahoo.com>
2019-04-29 08:33:35 +0100

    chore(endpoints): secure endpoints secured endpoints using joi update all locations of endpoints reference update endpoint readme [Finishes #165660309]

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -8,7 +8,7 @@ import {
 
 dotenv.config();
 
-const { API_VERSION } = process.env;
+const API_VERSION = '/api/v1';
 
 const userRoute = (app) => {
   app.post(`${API_VERSION}/auth/signup`, validateSignup, userController.signup);
